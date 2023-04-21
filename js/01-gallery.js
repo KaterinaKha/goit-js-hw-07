cac9b29b254543fc3602b1b20ae94762a508d2a9
KaterinaKha <katerinakhaa@gmail.com>
2023-04-21 21:14:18 +0200

    task 1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,52 @@
-import { galleryItems } from './gallery-items.js';
-// Change code below this line
+import { galleryItems } from "./gallery-items.js";
 
+const galleryItemElement = document.querySelector(".gallery");
+let instance;
+galleryItemElement.insertAdjacentHTML("beforeend", createGalleryItem(galleryItems));
+
+galleryItemElement.addEventListener("click", onImgClick);
+
+function createGalleryItem(galleryItems) {
+	return galleryItems
+		.map(({ preview, original, description }) => {
+			return `
+  <li class='gallery__item'>
+		<a class='gallery__link' href='${original}'>
+	<img 
+      class='gallery__image' 
+      src='${preview}' 
+      data-source='${original}' 
+      alt='${description}' />
+		</a>
+	</li>`;
+		})
+		.join(" ");
+}
+
+function onImgClick(e) {
+	e.preventDefault();
+
+	if (e.target.nodeName !== "IMG") {
+		return;
+	}
+
+	const imgSource = e.target.dataset.source;
+
+	const instance = basicLightbox.create(`
+    <div class="modal">
+    <img src="${imgSource}" />
+    </div>
+`);
+
+	instance.show();
+
+	document.addEventListener("keydown", closeImgCard);
+}
+
+function closeImgCard(e) {
+	if (e.code !== "Escape") return;
+
+	instance.close();
+	document.removeEventListener("keydown", closeImgCard);
+}
 console.log(galleryItems);
